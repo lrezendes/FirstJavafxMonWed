4abc4dfe3d5e19756086236b03e5d9ffbdbe275b
laurenrezendes <lrezendes@student.bridgew.edu>
2021-10-20 14:32:43 -0400

    Initial commit

---
+++ src/main/java/bsu/comp152/firstjavafxmonwed/HelloController.java
@@ -0,0 +1,21 @@
+package bsu.comp152.firstjavafxmonwed;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        var newFont = Font.font(24);
+        welcomeText.setFont(newFont);
+        var textColor = Paint.valueOf("Green");
+        welcomeText.setTextFill(textColor);
+        welcomeText.setText("Welcome to JavaFX Comp 152!!");
+
+    }
+}
